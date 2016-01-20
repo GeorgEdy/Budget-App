fd4acae0aed5000ffea329ea2649bfdf7906c9f0
razvan393 <razvanghinea05@gmail.com>
2016-01-20 23:10:12 +0200

    EditExpense

--- js/addExpense.js
+++ js/addExpense.js
@@ -1,7 +1,7 @@
 var addExpense = function (name, amount, category) {
     var totalBudget = budgetsStore.getAllBudget().budget;
 
-    spendingStore.addExpense({name: name, category: category, sum: amount});
+    expensesStore.addExpense({name: name, category: category, sum: amount});
     totalBudget = parseInt(totalBudget) - amount;
     budgetsStore.updateBudget(totalBudget);
 };
