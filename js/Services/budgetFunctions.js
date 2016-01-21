979429a69434adef5e09eeebdd326962e44f0579
Roxana Babatie <roxana.babatie@gmail.com>
2016-01-21 14:39:14 +0200

    added methods in budgetStore and added closure for budgetFunctions

--- js/Services/budgetFunctions.js
+++ js/Services/budgetFunctions.js
@@ -1,13 +1,15 @@
-var addBudget = function(name, amount, recurrent) {
-    budgetsStore.addBudget({name: name, sum: amount, recurrent: recurrent});
-}
+var budgetFunctions = function() {
+    var addBudget = function(name, amount, recurrent) {
+        budgetsStore.addBudget({name: name, sum: amount, recurrent: recurrent});
+    }
 
-var editBudget = function(id, name, amount, recurrent) {
-    var itemToEdit = budgetsStore.getBudget(id);
-    budgetsStore.updateBudget(itemToEdit.id, {name: name, amount: amount, recurrent: recurrent});
-}
+    var editBudget = function(id, name, amount, recurrent) {
+        var itemToEdit = budgetsStore.getBudget(id);
+        budgetsStore.updateBudget(itemToEdit.id, {name: name, amount: amount, recurrent: recurrent});
+    }
 
-var deleteBudget = function(id) {
-    var itemToDelete = budgetStore.getBudget(id);
-    budgetsStore.deleteBudget(itemToDelete.id);
-}
+    var deleteBudget = function(id) {
+        var itemToDelete = budgetStore.getBudget(id);
+        budgetsStore.deleteBudget(itemToDelete.id);
+    }
+}();
