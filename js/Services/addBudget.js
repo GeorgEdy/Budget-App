94712381c9459c5347ce7796591f133cfbd311d8
Roxana Babatie <roxana.babatie@gmail.com>
2016-01-21 14:15:53 +0200

    budget services

--- js/Services/addBudget.js
+++
@@ -1,6 +0,0 @@
-var addBudget = function(amount) {
-    var totalBudgetAmount = budgetsStore.getAllBudgets().budget;
-    totalBudgetAmount = parseInt(totalBudgetAmount) + parseInt(amount);
-    budgetsStore.updateBudget(totalBudgetAmount);
-
-}
---
+++ js/Services/budgetFunctions.js
@@ -0,0 +1,13 @@
+var addBudget = function(name, amount, recurrent) {
+    budgetsStore.addBudget({name: name, sum: amount, recurrent: recurrent});
+}
+
+var editBudget = function(id, name, amount, recurrent) {
+    var itemToEdit = budgetsStore.getBudget(id);
+    budgetsStore.updateBudget(itemToEdit.id, {name: name, amount: amount, recurrent: recurrent});
+}
+
+var deleteBudget = function(id) {
+    var itemToDelete = budgetStore.getBudget(id);
+    budgetsStore.deleteBudget(itemToDelete.id);
+}
