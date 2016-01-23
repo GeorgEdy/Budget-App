9d6abb4b8ee3b41f3a5111193ed15407ee9424f9
Ionut Firatoiu <firatoiu.ionut@gmail.com>
2016-01-23 13:05:10 +0200

    Promises and recuring store

---
+++ js/Stores/recurringStore.js
@@ -0,0 +1,68 @@
+var recurringStore = (function () {
+    var lastId = 2;
+    var data = [
+        {
+            id: 0,
+            name: "Salary",
+            category: "Income",
+            sum: "50",
+            recurrent: 0,
+            date: "15 01 2016"
+
+        },
+        {
+            id: 1,
+            name: "Electricity",
+            category: "Expenses",
+            sum: "100",
+            recurrent: 0,
+            date: "23 01 2016"
+        }
+    ];
+
+    return {
+        getAllRecurrings: function () {
+            return new Promise = (function(resolve, reject) {
+               resolve(data);
+            });
+        },
+        getRecurringById: function (id) {
+            return new Promise = (function (resolve, reject) {
+                var obj = "";
+                $.each(data, function (index) {
+                    if(index == id){
+                        obj = data[id];
+                    }
+                });
+                resolve(obj);
+            });
+        },
+        addRecurring: function (item) {
+            return new Promise = (function (resolve, reject){
+                item.id = lastId++;
+                data.push(item);
+                resolve(data);
+            });
+        },
+        updateRecurring: function (id, updateData) {
+            return new Promise = (function (resolve, reject) {
+                $.each(data, function () {
+                    if(this.id == id){
+                        data[id] = updateData;
+                    }
+                });
+                resolve(data);
+            });
+        },
+        deleteExpense: function (id) {
+            return new Promise = (function (resolve, reject) {
+                $.each(data, function (index) {
+                    if(this.id == id){
+                        data.splice(index, 1);
+                    }
+                });
+                resolve(data);
+            });
+        }
+    };
+})();
