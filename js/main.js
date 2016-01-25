4bd39f9407a28ed93db8cf05ea9a09255cde2415
GeorgEdy <eduard.dinu94@yahoo.ro>
2016-01-25 13:50:10 +0200

    devCss

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 $(function (){
     $('#categories').click(function(){
-        $('.show-categories').toggle();
+        $('.show-categories').attr('id','active');
     });
     addBudget(5, "caca-income", "income", "300", "income", "30.11.2015");
     addBudget(6, "caca-expense", "foods", "400", "expense", "30.11.2015");
 
-
+    $('#transactions').click(function(){
+        $('.show-categories').attr('id','');
+    });
+    $('#add-income').click(function(){
+        $('#income-form').addClass('active');
+        $('#expense-form').removeClass('active');
+    });
+    $('#add-expense').click(function(){
+        $('#income-form').removeClass('active');
+        $('#expense-form').addClass('active');
+    });
+    $('.total-income').click(function(){
+        $('#expense-history').toggle();
+    });
+    $('.total-expense').click(function(){
+        $('#expense-history').toggle();
+    });
 });
