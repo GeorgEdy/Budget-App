131082680d63b08e64907ce07103db04c67f4d8e
razvan393 <razvanghinea05@gmail.com>
2016-01-25 11:37:17 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -2,8 +2,4 @@ $(function (){
     $('#categories').click(function(){
         $('.show-categories').toggle();
     });
-    addBudget(5, "caca-income", "income", "300", "income", "30.11.2015");
-    addBudget(6, "caca-expense", "foods", "400", "expense", "30.11.2015");
-
-
 });
