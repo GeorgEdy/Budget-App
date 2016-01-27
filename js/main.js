85e9379c8b7a41cf609905986c5d3816317fa557
GeorgEdy <eduard.dinu94@yahoo.ro>
2016-01-27 13:32:35 +0200

    draw

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 var drawTable = function () {
     transactionsStore.getAllTransactions().then(function (data) {
         $('#income-history tbody tr').remove();
-        $.each(data, function () {
-            var tr = tmpl("item_tmpl", this);
-            $('#income-history tbody').append(tr);
-        });
+        if (data.type = "income") {
+            $.each(data, function () {
+                var tr = tmpl("item_tmpl", this);
+                $('#income-history tbody').append(tr);
+            });
+        }
+        else if(data.type ="expense") {
+            $.each(data, function () {
+                var tr = tmpl("item_tmpl", this);
+                $('#income-history tbody').append(tr);
+            });
+        }
     })
 };
 
@@ -14,7 +22,7 @@ var registerTransaction = function () {
     getTransactionData(parentNode, callbackTransactionData);
 };
 var callbackTransactionData = function (item, parentNode) {
-    if(item) {
+    if (item) {
         if (parentNode === "income-form") {
             item.type = "income";
             sendTransaction(item, item.recurring);
@@ -27,9 +35,19 @@ var callbackTransactionData = function (item, parentNode) {
 
 var sendTransaction = function (item, recurring) {
     if (recurring == true) {
-        recurringStore.addRecurring({name: item.name, categoryId: item.categoryId, sum: item.sum, type: item.type, date: item.date, recurringDate: item.recurringDate});
+        recurringStore.addRecurring({
+            name: item.name,
+            categoryId: item.categoryId,
+            sum: item.sum,
+            type: item.type,
+            date: item.date,
+            recurringDate: item.recurringDate
+        });
     } else {
-        addTransaction(item.name, item.categoryId, item.sum, item.type, item.date);
+        addTransaction(item.name, item.categoryId, item.sum, item.type, item.date).then(function () {
+           drawTable();
+        });
+
     }
 };
 
@@ -53,7 +71,7 @@ var getTransactionData = function (idForm, callbackTransactionData) {
     var cat = $('#' + idForm + ' [title = category]').val();
     var recurring = $('#' + idForm + ' [type = checkbox]').is(":checked");
     var date = moment().format('DD MM YYYY');
-    var recurringDate = $('#'+ idForm +' .datepicker').html();
+    var recurringDate = $('#' + idForm + ' .datepicker').html();
     resetErrors(idForm);
 
     if (validateTransactionData(idForm, name, sum, cat, recurringDate)) {
@@ -65,7 +83,14 @@ var getTransactionData = function (idForm, callbackTransactionData) {
                     return;
                 }
             });
-            callbackTransactionData({name: name, categoryId: categoryId, sum: sum, recurring: recurring, date: date, recurringDate: recurringDate }, idForm);
+            callbackTransactionData({
+                name: name,
+                categoryId: categoryId,
+                sum: sum,
+                recurring: recurring,
+                date: date,
+                recurringDate: recurringDate
+            }, idForm);
         });
     }
 };
@@ -119,32 +144,33 @@ $(function () {
     $('#home').click(function () {
         $('.show-categories').attr('id', '');
         $('.transactions').removeClass('index');
-
-        $('#add-income').click(function () {
-            $('#income-form').addClass('active');
-            $('#expense-form').removeClass('active');
-        });
-        $('#add-expense').click(function () {
-            $('#income-form').removeClass('active');
-            $('#expense-form').addClass('active');
-        });
-        $('.total-income').click(function () {
-            $('.income').addClass('active');
-            $('.expenses').removeClass('active');
-        });
-        $('.total-expense').click(function () {
-            $('.expenses').addClass('active');
-            $('.income').removeClass('active');
-        });
-        $('#transactions').click(function () {
-            $('.transactions').addClass('index');
-        });
-
-        $('#categories-form').submit(categoryOnSubmit);
-
-        //add income/expense
-
-        $('#income-form').submit(registerTransaction);
-        $('#expense-form').submit(registerTransaction);
     });
+    $('#add-income').click(function () {
+        $('#income-form').addClass('active');
+        $('#expense-form').removeClass('active');
+    });
+    $('#add-expense').click(function () {
+        $('#income-form').removeClass('active');
+        $('#expense-form').addClass('active');
+    });
+    $('.total-income').click(function () {
+        $('.income').addClass('active');
+        $('.expenses').removeClass('active');
+    });
+    $('.total-expense').click(function () {
+        $('.expenses').addClass('active');
+        $('.income').removeClass('active');
+    });
+    $('#transactions').click(function () {
+        $('.transactions').addClass('index');
+    });
+
+    $('#categories-form').submit(categoryOnSubmit);
+
+    //add income/expense
+
+    $('#income-form').submit(registerTransaction);
+    $('#expense-form').submit(registerTransaction);
+
+    drawTable();
 });
